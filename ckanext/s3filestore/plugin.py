f819feb488cd763c776d855a93e0f3f29fd9febd
jbrown-xentity <jbrown@xentity.com>
2020-11-11 12:34:07 -0700

    update config_options

--- ckanext/s3filestore/plugin.py
+++ ckanext/s3filestore/plugin.py
@@ -26,11 +26,11 @@ class S3FileStorePlugin(plugins.SingletonPlugin):
            'ckanext.s3filestore.aws_bucket_name'
         )
         if not config.get('ckanext.s3filestore.aws_use_iam_role'):
-            config_options += ('ckanext.s3filestore.aws_access_key_id')
-            config_options += ('ckanext.s3filestore.aws_secret_access_key')
-            config_options += ('ckanext.s3filestore.region_name')
-            config_options += ('ckanext.s3filestore.signature_version')
-            config_options += ('ckanext.s3filestore.host_name')
+            config_options = config_options + ('ckanext.s3filestore.aws_access_key_id')
+            config_options = config_options + ('ckanext.s3filestore.aws_secret_access_key')
+            config_options = config_options + ('ckanext.s3filestore.region_name')
+            config_options = config_options + ('ckanext.s3filestore.signature_version')
+            config_options = config_options + ('ckanext.s3filestore.host_name')
         for option in config_options:
             if not config.get(option, None):
                 raise RuntimeError(missing_config.format(option))
