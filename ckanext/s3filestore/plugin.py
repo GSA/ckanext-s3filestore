e037520fa437aa93d7f2515ba37c851f541fbc66
jbrown-xentity <jbrown@xentity.com>
2020-11-11 12:38:21 -0700

    config_options to list

--- ckanext/s3filestore/plugin.py
+++ ckanext/s3filestore/plugin.py
@@ -22,15 +22,15 @@ class S3FileStorePlugin(plugins.SingletonPlugin):
         # Certain config options must exists for the plugin to work. Raise an
         # exception if they're missing.
         missing_config = "{0} is not configured. Please amend your .ini file."
-        config_options = (
-           'ckanext.s3filestore.aws_bucket_name'
-        )
+        config_options = ['ckanext.s3filestore.aws_bucket_name']
         if not config.get('ckanext.s3filestore.aws_use_iam_role'):
-            config_options = config_options + ('ckanext.s3filestore.aws_access_key_id')
-            config_options = config_options + ('ckanext.s3filestore.aws_secret_access_key')
-            config_options = config_options + ('ckanext.s3filestore.region_name')
-            config_options = config_options + ('ckanext.s3filestore.signature_version')
-            config_options = config_options + ('ckanext.s3filestore.host_name')
+            config_options.append([
+                'ckanext.s3filestore.aws_access_key_id',
+                'ckanext.s3filestore.aws_secret_access_key',
+                'ckanext.s3filestore.region_name',
+                'ckanext.s3filestore.signature_version',
+                'ckanext.s3filestore.host_name'
+            ])
         for option in config_options:
             if not config.get(option, None):
                 raise RuntimeError(missing_config.format(option))
